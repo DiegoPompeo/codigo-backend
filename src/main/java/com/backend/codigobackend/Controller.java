d5fec474be3ed65b16eb4addb57bc0c325e297f9
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-03 00:26:34 -0300

    response entity

--- src/main/java/com/backend/codigobackend/Controller.java
+++ src/main/java/com/backend/codigobackend/Controller.java
@@ -1,6 +1,7 @@
 package com.backend.codigobackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -31,8 +32,8 @@ public class Controller {
     }
 
     @PostMapping("/signup")
-    public Pessoa adiciona(@Valid @RequestBody Pessoa p) {
-        return service.add(p);
+    public ResponseEntity<Pessoa> adiciona(@Valid @RequestBody Pessoa p) {
+        return ResponseEntity.ok(service.add(p));
     }
 
     @PostMapping("/login")
@@ -106,8 +107,8 @@ public class Controller {
     }
 
     @PostMapping("/addPost")
-    public Post addPost(@RequestBody Post post) {
-        return postService.add(post);
+    public ResponseEntity<Post> addPost(@RequestBody Post post) {
+        return ResponseEntity.ok(postService.add(post));
     }
 
     @GetMapping("/verPost/{email}")
@@ -137,40 +138,4 @@ public class Controller {
         return glossarioService.listar();
     }
 
-    @PostMapping("/addGlossario")
-    public void addGlossario(){
-
-        ArrayList<String> lista = new ArrayList<>(
-                Arrays.asList(
-                        "AngularJS", "Inteligência Artificial", "Retropropagação",
-                        "Teorema de Bayes", "Rede bayesiana", "Viés", "Big Data", "Distribuição Binomial",
-                        "Teste Qui-Quadrado", "Classificação", "Agrupamento", "Coeficiente", "Linguística Computacional",
-                        "Intervalo de Confiança", "Variável Contínua", "Correlação", "Covariância", "Validação Cruzada",
-                        "D3", "Engenheiro de Dados", "Mineração de Dados", "Ciência de Dados", "Estrutura de Dados",
-                        "Disputa de Dados", "Árvores de Decisão", "Aprendizagem Profunda", "Variável Dependente",
-                        "Redução de Dimensão", "Variável Discreta", "Econometria", "Característica", "Engenharia de Recursos",
-                        "PORTÃO", "Aumento de Gradiente", "Gradiente Descendente", "Histograma", "Variável Independente",
-                        "Javascript", "Cluster de K-significa", "K Vizinhos mais Próximos", "Variavel Latente", "Lift",
-                        "Álgebra Linear", "Regressão Linear", "Logaritmo", "Regressão Logística", "Aprendizado de Máquina",
-                        "Modelo de Aprendizado de Máquina", "Cadeia de Markov", "MATLAB", "Matriz", "Significar",
-                        "Erro Absoluto Médio", "Erro Médio Quadrático", "Mediana", "Modo", "Modelo", "Método de Monte Carlo",
-                        "Média Móvel", "N-grama", "Classificador Ingênuo de Bayes", "Rede Neural", "Distribuição Normal",
-                        "NoSQL", "Hipótese Nula", "Função Objetiva", "Ponto Fora da Curva", "Overfitting", "Valor P",
-                        "Ranking da Página", "Pandas", "Perceptron", "Perl", "Tabela Dinâmica", "Distribuição de Veneno",
-                        "Distribuição Posterior", "Análise Preditiva", "Modelagem Preditiva", "Análise do Componente Principal",
-                        "Distribuição Prévia", "Distribuição de Probabilidade", "Pitão", "Quantil, Quartil", "R", "Floresta Aleatória",
-                        "Regressão", "Aprendizado por Reforço", "Erro Médio Quadrático da Raiz", "Rubi", "Curva S", "SAS", "Escalar",
-                        "Scripting", "Correlação Serial", "Concha", "Dados Espaço-Temporais", "SPSS", "SQL", "Desvio Padrão",
-                        "Distribuição Normal Padrão", "Pontuação Padronizada", "Stata", "Estratos, Amostragem Estratificada",
-                        "Aprendizado Supervisionado", "Máquina de Vetor de Suporte", "Distribuição T", "Quadro",
-                        "Dados de Séries Temporais", "UIMA", "Aprendizado não Supervisionado", "Variação", "Vetor", "Espaço Vetorial",
-                        "Weka"
-                )
-        );
-        lista.forEach(data -> {
-            Glossario g = new Glossario();
-            g.setNome(data);
-            glossarioService.add(g);
-        });
-    }
 }
