bbd90d85cc2bfa4629efe79c8fe6cf48caaaa92e
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-15 17:33:27 -0300

    PACKAGE-PRIVATE CONTROLLER

--- src/main/java/com/backend/codigobackend/Controller.java
+++ src/main/java/com/backend/codigobackend/Controller.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/redesocial")
-public class Controller {
+class Controller {
     @Autowired
     private PessoaService service;
 
