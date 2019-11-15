3d770ce0d0bca8b36f0bea505fb085c4bda45ed4
Diego C. Pompeo <54604717+DiegoPompeo@users.noreply.github.com>
2019-11-15 17:25:13 -0300

    pessoaservice

--- src/main/java/com/backend/codigobackend/PessoaService.java
+++ src/main/java/com/backend/codigobackend/PessoaService.java
@@ -1,12 +1,31 @@
 package com.backend.codigobackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-public interface PessoaService {
-    List<Pessoa> listar();
-    Pessoa listarId(int id);
-    Pessoa add(Pessoa p);
-    Pessoa edit(Pessoa p);
-    List<Pessoa> findNome(String nome);
-    boolean findByEmail(String email);
+@Service
+class PessoaService {
+    @Autowired
+    private PessoaRepository repository;
+
+    List<Pessoa> listar(){
+        return repository.findAll();
+    }
+    Pessoa listarId(int id){
+        return repository.findById(id);
+    }
+    Pessoa add(Pessoa p){
+        return repository.save(p);
+    }
+    Pessoa edit(Pessoa p){
+        return repository.save(p);
+    }
+    List<Pessoa> findNome(String nome){
+        return repository.findByNome(nome);
+    }
+    boolean findByEmail(String email){
+        return repository.findByEmail(email);
+    }
 }
--- src/main/java/com/backend/codigobackend/PostServiceImp.java
+++
@@ -1,5 +0,0 @@
-package com.backend.codigobackend;
-
-public class PostServiceImp {
-
-}
